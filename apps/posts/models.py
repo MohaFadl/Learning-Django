ff5697f4466d2179283bf741e8064adf77b7f2ca
mohaFadl <mohamed.medhat.fadl@gmail.com>
2025-09-06 20:39:34 +0300

    Added Simple Front for the APIs & Using Canonical to provide seo friendly APIs

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
     #A nice way to do TextChoices
     
     class Status(models.TextChoices):
@@ -12,11 +19,13 @@ class Post(models.Model):
         
         
     title = models.CharField(max_length = 250)
-    slug = models.SlugField(max_length = 250)
+    slug = models.SlugField(max_length = 250 , unique_for_date='published')
+    content = models.TextField()
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)   
+    updated = models.DateTimeField(auto_now=True)
+
     """
     another method we can use now from db instead of Python itself
     by adding 
@@ -47,4 +56,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('posts:post_detail',args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
     
+
+
